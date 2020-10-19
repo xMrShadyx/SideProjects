612c3cdb74ea274edf821847f8573b81c5dbb623
xMrShadyx <daredevil91138@gmail.com>
2020-10-19 08:07:18 +0300

    Youtube Video -> to mp3, and keep the video

---
+++ Side-Projects/YoutubeDownloader/YoutubeDown.py
@@ -0,0 +1,43 @@
+import os
+from pytube import YouTube #pip install pytube3
+from moviepy.editor import * #pip install moviepy **Requires numpy!!!!
+from time import sleep
+
+playlist = ['https://youtu.be/ZWuwJQPUmnw']
+
+
+for item in playlist:
+    url = item
+    my_video = YouTube(url)
+    print("****Video Title****")
+    print(my_video.title)
+    print("***Download video***")
+    my_video = my_video.streams.get_highest_resolution()
+    my_video.download()
+
+
+for file in os.listdir("E:\\Coding\\PyProject\\Side-Projects\\YoutubeDownloader"):
+    if file.endswith(".mp4"):
+        os.rename(file, file.replace(" ", "").replace("-", "").replace("!", "").replace("(", "").replace(")", ""))
+
+list_n = []
+for file in os.listdir("E:\\Coding\\PyProject\\Side-Projects\\YoutubeDownloader"):
+    if file.endswith(".mp4"):
+        list_n.append(file)
+
+
+for item in list_n:
+    print(item)
+    mp4_file = r'{}'.format(item)
+    mp3_file = r'{}.{}'.format(item,'mp3')
+
+    videoclip = VideoFileClip(mp4_file)
+    audioclip = videoclip.audio
+    audioclip.write_audiofile(mp3_file)
+    audioclip.close()
+    videoclip.close()
+
+
+
+
+
