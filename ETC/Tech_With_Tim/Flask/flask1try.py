2c454a78ca2ee42dca0ac534c3fa5defd064d32c
xMrShadyx <daredevil91138@gmail.com>
2020-11-02 11:21:30 +0200

    v0.0.4 Sessions

--- ETC/Tech_With_Tim/Flask/flask1try.py
+++ ETC/Tech_With_Tim/Flask/flask1try.py
@@ -1,24 +1,40 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session
+from datetime import timedelta
+
 
 app = Flask(__name__)
-
+app.secret_key = "ThisStringMustBeTheMostHardestThingToBreakEverInTheWorldReally..."
+app.permanent_session_lifetime = timedelta(minutes=20)
 
 @app.route("/")
-def home(name):
+def home():
     return render_template("index.html")
 
 @app.route("/login", methods=['POST','GET'])
 def login():
     if request.method == 'POST':
+        session.permanent = True
         user = request.form["nm"]
-        return redirect(url_for("user", usr=user))
+        session["user"] = user
+        return redirect(url_for("user"))
     else:
+        if "user" in session:
+            return redirect(url_for("user"))
+
         return render_template("login.html")
 
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return f"<h1>{user}</h1>"
+    else:
+        return redirect(url_for("login"))
 
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
 
 if  __name__ == '__main__':
     app.run(debug=True)
