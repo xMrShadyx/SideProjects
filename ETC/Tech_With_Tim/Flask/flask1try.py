7ca1c9aa5985344d9da6625865c6e4d4b55e6fe1
xMrShadyx <daredevil91138@gmail.com>
2020-11-02 11:46:58 +0200

    v0.0.5

--- ETC/Tech_With_Tim/Flask/flask1try.py
+++ ETC/Tech_With_Tim/Flask/flask1try.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, request, session
+from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
 
 
@@ -16,9 +16,11 @@ def login():
         session.permanent = True
         user = request.form["nm"]
         session["user"] = user
+        flash("Login Succesfully!!")
         return redirect(url_for("user"))
     else:
         if "user" in session:
+            flash("Already Logged in!")
             return redirect(url_for("user"))
 
         return render_template("login.html")
@@ -27,12 +29,16 @@ def login():
 def user():
     if "user" in session:
         user = session["user"]
-        return f"<h1>{user}</h1>"
+        return render_template("user.html", user=user)
     else:
+        flash(f"You are not logged in!")
         return redirect(url_for("login"))
 
 @app.route("/logout")
 def logout():
+    if "user" in session:
+        user = session["user"]
+        flash(f"You have been logged out, {user}", "info")
     session.pop("user", None)
     return redirect(url_for("login"))
 
