84637a69ba0c3dd15ca5e49861b95c6cf24af409
xMrShadyx <daredevil91138@gmail.com>
2020-11-02 10:20:21 +0200

    Added bootstrap

--- ETC/Tech_With_Tim/Flask/flask1try.py
+++ ETC/Tech_With_Tim/Flask/flask1try.py
@@ -3,10 +3,10 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 
-@app.route("/<name>/")
+@app.route("/")
 def home(name):
-    return render_template("index.html", content=["tim", "joe", "bill"])
+    return render_template("index.html")
 
 
 if  __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
