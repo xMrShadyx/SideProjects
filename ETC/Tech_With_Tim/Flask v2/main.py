7ad75960edcb90a2d431b9ebd76c5f32f5e94607
xMrShadyx <daredevil91138@gmail.com>
2020-11-02 17:53:32 +0200

    Blueprints & Using Multiple Python Files

--- ETC/Tech_With_Tim/Flask v2/main.py
+++ ETC/Tech_With_Tim/Flask v2/main.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
+from admin.second import second
 
 app = Flask(__name__)
+app.register_blueprint(second, url_prefix="/admin")
 
-@app.route("/home")
 @app.route("/")
-def home():
-    return render_template("home.html")
+def test():
+    return "<h1>Test</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
