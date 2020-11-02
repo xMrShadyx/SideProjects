9597dd38fd29036037fee81731f00335963dfa79
xMrShadyx <daredevil91138@gmail.com>
2020-11-02 10:38:03 +0200

    v0.0.3

--- ETC/Tech_With_Tim/Flask/flask1try.py
+++ ETC/Tech_With_Tim/Flask/flask1try.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,18 @@ app = Flask(__name__)
 @app.route("/")
 def home(name):
     return render_template("index.html")
+
+@app.route("/login", methods=['POST','GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 
 if  __name__ == '__main__':
