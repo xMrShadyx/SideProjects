efd99e93a9fc99d650b8fd2a77aa73933cde6835
xMrShadyx <daredevil91138@gmail.com>
2020-11-04 13:53:51 +0200

    Paginations

--- ETC/CoreyShafer/Flask/BLOG/flaskblog/routes.py
+++ ETC/CoreyShafer/Flask/BLOG/flaskblog/routes.py
@@ -11,7 +11,8 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/")
 @app.route("/home")
 def home():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
 
 
@@ -138,3 +139,13 @@ def delete_post(post_id):
     db.session.commit()
     flash("Your post has been deleted!", 'success')
     return redirect(url_for('home'))
+
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.filter_by(author=user)\
+    .order_by(Post.date_posted.desc())\
+    .paginate(page=page, per_page=5)
+    return render_template('user_posts.html', posts=posts, user=user)
