d3024755315895a27b75ac5fed55bda03380e480
xMrShadyx <daredevil91138@gmail.com>
2020-11-05 07:42:12 +0200

    PW Rec sys.

--- ETC/CoreyShafer/Flask/BLOG/flaskblog/__init__.py
+++ ETC/CoreyShafer/Flask/BLOG/flaskblog/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
@@ -12,5 +13,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'shady91q'
+app.config['MAIL_PASSWORD'] = 'daredevil911'
+mail = Mail(app)
 
 from flaskblog import routes
--- ETC/CoreyShafer/Flask/BLOG/flaskblog/models.py
+++ ETC/CoreyShafer/Flask/BLOG/flaskblog/models.py
@@ -1,5 +1,6 @@
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from datetime import datetime
-from flaskblog import db, login_manager
+from flaskblog import db, login_manager, app
 from flask_login import UserMixin
 
 
@@ -15,6 +16,19 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
     rank = db.Column(db.String(10), nullable=False, default='user')
     posts = db.relationship('Post', backref='author', lazy=True)
+
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({"user_id": self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
 
     def __repr__(self):
         return f"User: ('{self.username}, {self.email}, {self.image_file}')"
