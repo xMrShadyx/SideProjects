5c237ad34d7932a06002fb37f5a3fbd96f771397
xMrShadyx <daredevil91138@gmail.com>
2020-11-05 07:46:47 +0200

    PWS 2

--- ETC/CoreyShafer/Flask/BLOG/flaskblog/__init__.py
+++ ETC/CoreyShafer/Flask/BLOG/flaskblog/__init__.py
@@ -14,11 +14,12 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
+# Mail Sender
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'username' # with out @gmail.com
-app.config['MAIL_PASSWORD'] = 'password'
+app.config['MAIL_PASSWORD'] = 'password' # pasword
 mail = Mail(app)
 
 from flaskblog import routes
